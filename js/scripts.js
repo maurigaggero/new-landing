e03664d99f8a8854c04a3b4acf20783df1eb00ba
Mauricio Gaggero <mauricio.gaggero@hotmail.com>
2025-12-10 18:07:24 -0300

    add carousel de verificados

--- js/scripts.js
+++ js/scripts.js
@@ -74,3 +74,73 @@ window.addEventListener("scroll", () => {
         bg.style.backgroundPositionY = scroll * 0.4 + "px";
     });
 });
+
+// Verificados
+
+const carousel = document.getElementById("carouselVerificados");
+
+carousel.addEventListener("scroll", () => {
+    const cards = carousel.querySelectorAll(".carousel-card");
+    cards.forEach(card => {
+        const rect = card.getBoundingClientRect();
+        const offset = (window.innerWidth / 2 - rect.left) * 0.03;
+        card.style.transform = `perspective(900px) rotateY(${offset}deg)`;
+    });
+});
+
+async function getVerificados(limit = 12) {
+    try {
+        const res = await fetch('https://carwatcherapi.azurewebsites.net/api/verificacion?pageNumber=1&pageSize=' + limit);
+        const data = await res.json();
+
+        return data.items || [];
+
+    } catch (err) {
+        console.error('Error al obtener verificaciones:', err);
+        return [];
+    }
+}
+
+async function renderVerificados() {
+    const container = document.getElementById('carouselVerificados');
+    container.innerHTML = "";
+
+    const verificaciones = await getVerificados(12);
+
+    if (verificaciones.length === 0) {
+        container.innerHTML = `
+            <p class="text-center text-gray-500 mt-3 w-full">
+                No hay vehículos disponibles en este momento.
+            </p>`;
+        return;
+    }
+
+    verificaciones.forEach(v => {
+        const div = document.createElement('div');
+        div.className = `
+        bg-white rounded-2xl overflow-hidden shadow-lg
+        hover:shadow-2xl hover:-translate-y-[3px]
+        transition-all duration-300 cursor-pointer card-hover
+        flex-shrink-0 snap-start
+        w-[80%] sm:w-[300px] md:w-[220px]
+    `;
+
+        const link = document.createElement('a');
+        link.href = `https://app.ghia.com.ar/verificados`;
+        link.style.textDecoration = 'none';
+        link.style.color = 'inherit';
+
+        link.innerHTML = `
+        <img src="${v.rutaArchivos[0]}" class="h-40 w-full object-cover" alt="Foto de ${v.dominio}">
+        <div class="p-3">
+            <h6 class="font-semibold text-gray-900 mb-1">${v.marca} ${v.modelo}</h6>
+            <small class="text-gray-500">${v.anio} - ${v.kilometraje} km</small>
+        </div>
+    `;
+
+        div.appendChild(link);
+        container.appendChild(div);
+    });
+};
+
+document.addEventListener('DOMContentLoaded', renderVerificados);
